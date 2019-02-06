1aad380dd179fba0cad84ab9aa761e425bfaa4b1
Matthew Kobs <matt.kobs@cph.org>
2019-02-05 21:09:11 -0600

    [skip] Removed storage of state (3m)

--- background.js
+++ background.js
@@ -1,8 +1,4 @@
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({ speed: 1.0 }, function() {
-    console.log("Playback set to 1x");
-  });
-
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [ new chrome.declarativeContent.PageStateMatcher({
--- popup.js
+++ popup.js
@@ -1,14 +1,3 @@
-function updateActiveForSpeed(speed) {
-  let oldElement = document.querySelector('.active');
-  if(oldElement) oldElement.classList.remove('active');
-  let activeElement = document.querySelector('a[data-speed="' + speed.toFixed(1) + '"]');
-  if(activeElement) activeElement.classList.add('active');
-}
-
-chrome.storage.sync.get('speed', function(data) {
-  updateActiveForSpeed(data.speed);
-});
-
 document.querySelectorAll('a[data-speed]').forEach(function(speedLink) {
   speedLink.onclick = function(e) {
     let speed = parseFloat(e.target.getAttribute('data-speed'));
@@ -17,8 +6,5 @@ document.querySelectorAll('a[data-speed]').forEach(function(speedLink) {
         code: 'document.querySelectorAll("audio").forEach(function(audio) { audio.playbackRate = ' + speed + '; });'
       });
     });
-
-    chrome.storage.sync.set({ speed: speed });
-    updateActiveForSpeed(speed);
   }
 });
